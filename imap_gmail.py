3dbcbdc7c84a2d6f6cc1679cbb00b5c0495757cb
Ludovic Picard <luldopic@googlemail.com>
2022-02-17 18:47:37 +0100

    Moving Frame Classes to seperate py file

--- imap_gmail.py
+++ imap_gmail.py
@@ -4,7 +4,7 @@
 
 import imaplib
  
-class gmail_imap:
+class imap:
  
     def __init__ (self, username, password):
         self.imap_server = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -12,8 +12,7 @@ class gmail_imap:
         self.password = password
         self.loggedIn = False
  
-        self.mailboxes = gmail_mailboxes(self.imap_server);
-        self.messages = gmail_messages.gmail_messages(self);
+        self.mailboxes = mailboxes(self.imap_server);
  
     def login (self):
         self.imap_server.login(self.username,self.password)
@@ -25,7 +24,7 @@ class gmail_imap:
         self.loggedIn = False
     
 
-class gmail_mailboxes:
+class mailboxes:
     def __init__(self, gmail_server):
         self.server = gmail_server
         self.mailboxes = list()
@@ -45,7 +44,7 @@ class gmail_mailboxes:
     def __getitem__(self, key): return self.mailboxes[key]
 
 
-class gmail_messages:
+class messages:
     def __init__(self):
         pass
     def __init__(self, gmail_server):
--- main.py
+++ main.py
@@ -6,50 +6,10 @@ Primary Email Sorting Interface
 """
 
 import tkinter as tk
-from tkinter import ttk
-import requests
 import json
 import imap_gmail
+import AppFrame
 
-class mailboxFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-
-class emailFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-
-class sortingFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-        self.columnconfigure(0, weight=1)
-        self.__create_widget()
-    def __create_widgets(self):
-        Actionable = tk.StringVar(self) #Actionable vs Non-actionable
-        Actionable.set("Actionable")
-        Urgency = tk.StringVar(self) #High(1-2 day) vs Medium(3-7 days) vs Low (>7 days)
-        Urgency.set("Medium (3-7 days)")
-        Purpose = tk.StringVar(self) #Personal vs Business
-        Purpose.set("Business")
-        Action = tk.StringVar(self) #Labelled vs Deleted
-        Action.set("Labelled")
-        
-        paddings = {'padx': 5, 'pady':5}
-        
-        Actionable_Menu = ttk.OptionMenu(self, Actionable, "Actionable", "Non-actionable")
-        Actionable_Menu.pack(**paddings)
-        Urgency_Menu = ttk.OptionMenu(self, Actionable, "High(1-2 days)", "Medium(3-7 days)","Low(>7 days)")
-        Urgency_Menu.pack(**paddings)
-        Purpose_Menu = ttk.OptionMenu(self, Actionable, "Personal", "Business")
-        Purpose_Menu.pack(**paddings)
-        Action_Menu = ttk.OptionMenu(self, Actionable, "To label", "To delete")
-        Action_Menu.pack(**paddings)
-
-class TopbuttonFrame(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-        
-        
 
 class App(tk.Tk):
     def __init__(self,root):
@@ -64,8 +24,11 @@ class App(tk.Tk):
         root.resizable(width=False, height=False)
         
     
-    def fetchEmail(self):
-        pass
+    def getMail(self):
+        credentialsJSON = "credentialsGoogle.json"
+        with open(credentialsJSON) as handler:
+            credentials = json.load(handler)
+        mail = imap_gmail.imap(credentials["user"], credentials["password"])
     
 if __name__ == '__main__':
     app = App()
