fd2e7fa0fd27901bd6dcb89afde49dafba3b27f1
Ludovic Picard <luldopic@googlemail.com>
2022-02-18 16:53:17 +0100

    Properly Setting up Inheritance

--- imap_gmail.py
+++ imap_gmail.py
@@ -4,36 +4,34 @@
 import imaplib
 import json
 
-class gmail:
+class gmail(imaplib.IMAP4_SSL):
     def __init__(self):
-        super().__init__(self)
+        super().__init__("imap.gmail.com")
         self.loggedIn = False
  
     def login (self):
-        while loggedIn == False:
+        while self.loggedIn == False:
             try:
                 credentialsJSON = "credentialsGoogle.json"
                 with open(credentialsJSON) as handler:
                     credentials = json.load(handler)
-                result = self.imap_server.login(credentials["user"],credentials["password"])
+                result = super().login(credentials["user"],credentials["password"])
                 if result[0] == 'OK':
                     self.loggedIn = True
             except:
                 print("Log in Failed")
          
     def logout (self):
-        self.imap_server.close()
-        self.imap_server.logout()
+        self.close()
+        super().logout()
         self.loggedIn = False
     
-    def get_all_mailboxes(self):
-        self.AllMailboxes = mailboxes()
-        for mailbox in 
-    
+    def get_all_mail(self): return mailboxes("[Gmail]/All Mail")
 
 class mailboxes:
     def __init__(self, **name):
-        self.mailboxes = list()
+        self.mailboxes = list(name)
+        self.name = name
  
     def load(self):
         if(not self.server.loggedIn):
@@ -44,16 +42,16 @@ class mailboxes:
             if( name != "[Gmail]"):  #ignore global [Gmail] mailbox
                 self.mailboxes.append(name)
  
-    def __repr__(self):
-        return "<gmail_mailboxes: [%s]="">" %  (',  '.join(self.mailboxes))
- 
     def __getitem__(self, key): return self.mailboxes[key]
 
 def connectToGmail():
     handshake = False
     while handshake == False:
         try:
+            server = gmail()
             handshake = True
-            return gmail(imaplib.IMAP4_SSL("imap.gmail.com"))
+            print("Handshake Succeed")
+            return server
+            
         except:
             print("Handshake Failed")
