26812bcfda0f7a2945d06ead594e16f28c547842
Ludovic Picard <luldopic@googlemail.com>
2022-02-18 14:14:22 +0100

    Changing gmail class to be a child of IMAP4_SSL object

--- imap_gmail.py
+++ imap_gmail.py
@@ -4,13 +4,12 @@
 
 import imaplib
 import json
- 
+
 class gmail:
-    def __init__ (self):
-        self.imap_server = imaplib.IMAP4_SSL("imap.gmail.com")
+    def __init__(self):
+        super().__init__(self)
         self.loggedIn = False
- 
-        self.mailboxes = mailboxes(self.imap_server);
+        self.mailboxlist = self.list();
  
     def login (self):
         credentialsJSON = "credentialsGoogle.json"
@@ -28,7 +27,6 @@ class gmail:
 
 class mailboxes:
     def __init__(self, gmail_server):
-        super.__init__(self)
         self.server = gmail_server
         self.mailboxes = list()
  
@@ -46,22 +44,11 @@ class mailboxes:
  
     def __getitem__(self, key): return self.mailboxes[key]
 
-    def __init__(self):
-        pass
-    def __init__(self, gmail_server):
-        self.server = gmail_server
-        self.mailboxes = list()
- 
-    def load(self):
-        if(not self.server.loggedIn):
-            self.server.login()
- 
-        for box in self.server.imap_server.list()[1]:  #Ugly
-            name = box.split(' "/" ')[1][1:-1]
-            if( name != "[Gmail]"):  #ignore global [Gmail] mailbox
-                self.mailboxes.append(name)
- 
-    def __repr__(self):
-        return "<gmail_mailboxes: [%s]="">" %  (',  '.join(self.mailboxes))
- 
-    def __getitem__(self, key): return self.mailboxes[key]
+def connectToGmail():
+    handshake = False
+    while handshake == False:
+        try:
+            handshake = True
+            return gmail(imaplib.IMAP4_SSL("imap.gmail.com"))
+        except:
+            print("Handshake Failed")
