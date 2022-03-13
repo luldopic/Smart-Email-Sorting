0041989e1fa26335ad396186702d95ef751b4c73
Ludovic Picard <luldopic@googlemail.com>
2022-03-13 17:16:15 +0000

    Update mailDB.py

--- mailDB.py
+++ mailDB.py
@@ -7,26 +7,36 @@ Communicate with Local SQL Server to sort emails
 import mysql.connector
 import json
 
-class database:
-    def __init__(self):
-        get
 
-    def getDBName(self):
+class myDB:
+    def __init__(self, database_name):
+        self.createDB(database_name)
+
+    def createDB(self, database_name):
+        self.__connectToSQL__(database_name)
+        if self.db_exists != True:
+            cursor = self.db.cursor()
+            cursor.execute("CREATE DATABASE %s", database_name)
+        else:
+            pass
+
+    def createTable(self, table_name, table_columns):
+        pass
+
+    def __connectToSQL__(self, database_name):
         with open("MySQLcredentials.json") as handler:
             try:
                 credentials = json.load(handler)
-                self.db_name = credentials["database"]
+                self.db = mysql.connector.connect(host=credentials["host"], user=credentials["user"],
+                                                  password=credentials["password"], database=database_name)
+                del credentials
+                print("Successful Connection")
             except Exception as e:
-                print(e)
-                
-    def connectToSQL():
-        with open("MySQLcredentials.json") as handler:
-            try:
-                credentials = json.load(handler)
-                db = mysql.connector.connect(host=credentials["host"],user=credentials["user"],password = credentials["password"])
-                self.db_name = credentials[db]
-                del(credentials)
-            except Exception as e:
-                pass
+                error = str(e)
+                if error.__contains__("Unknown Database"):
+                    self.db_exists = False
+                else:
+                    print(e)
 
-test = connectToSQL()
+
+test = myDB("Gmail Mail")
