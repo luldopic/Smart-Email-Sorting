6f17bba983196eb23e060196b48bb3cd783e4085
Ludovic Picard <luldopic@googlemail.com>
2022-03-13 20:36:40 +0000

    Update mailDB.py

--- mailDB.py
+++ mailDB.py
@@ -22,11 +22,38 @@ class emailDB:
             self.cursor.execute("CREATE DATABASE GmailMail")
 
     def createTable(self, table_name, table_columns=None):
+        if not self.checkTableExist(table_name):
+            try:
+                SQL = "CREATE TABLE {name} (id INT AUTO_INCREMENT PRIMARY KEY);".format(name=table_name)
+                self.cursor.execute(SQL)
+            except Exception as e:
+                print(e)
+        else:
+            pass
+
+    def checkTableExist(self, table_name):
         try:
-            SQL = "CREATE TABLE {name} (id INT AUTO_INCREMENT PRIMARY KEY);".format(name = table_name)
+            SQL = "SHOW TABLES LIKE '{name}'".format(name=table_name)
             self.cursor.execute(SQL)
+            if self.cursor.arraysize != 0:
+                print("Table exists")
+                return True
+            else: return False
         except Exception as e:
             print(e)
+
+    def addTableColumn(self, table_name, table_column):
+        SQL = """
+            IF NOT EXISTS( 
+                SELECT
+                    column_name
+                FROM
+                    INFORMATION.SCHEMA.COLUMNS
+                WHERE
+                    TABLE_NAME = '{name}'
+                    AND COLUMN_NAME = '{column_name}')
+                ALTER TABLE {name} ADD ({column_name} {column_type});""".format(name=table_name, column_name=table_column[0], column_type = table_column[1])
+        self.cursor.execute(SQL)
 
     def __connectToSQL__(self, db_exist=True):
         with open("MySQLcredentials.json") as handler:
