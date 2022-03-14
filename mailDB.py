767e48cd5e2084905b600c575129d69c14ba0f6e
Ludovic Picard <luldopic@googlemail.com>
2022-03-14 15:07:47 +0000

    Update mailDB.py

--- mailDB.py
+++ mailDB.py
@@ -11,49 +11,67 @@ import json
 class emailDB:
     def __init__(self):
         self.connectDB()
-        self.cursor = self.db.cursor()
 
     def connectDB(self):
         try:
             self.__connectToSQL__()
         except UnknownDatabase:
             self.__connectToSQL__(db_exist=False)
-            self.cursor = self.db.cursor()
-            self.cursor.execute("CREATE DATABASE GmailMail")
+            SQL = "CREATE DATABASE GmailMail"
+            self.executeSQLCursor(SQL)
 
     def createTable(self, table_name, table_columns=None):
-        if not self.checkTableExist(table_name):
-            try:
-                SQL = "CREATE TABLE {name} (id INT AUTO_INCREMENT PRIMARY KEY);".format(name=table_name)
-                self.cursor.execute(SQL)
-            except Exception as e:
-                print(e)
-        else:
-            pass
-
-    def checkTableExist(self, table_name):
         try:
-            SQL = "SHOW TABLES LIKE '{name}'".format(name=table_name)
-            self.cursor.execute(SQL)
-            if self.cursor.arraysize != 0:
-                print("Table exists")
-                return True
-            else: return False
+            self.__checkTableExist__(table_name)
+        except UnknownTable:
+            SQL = "CREATE TABLE {name} (id INT AUTO_INCREMENT PRIMARY KEY);".format(name=table_name)
+            self.executeSQLCursor(SQL)
+        except TableAlreadyCreated:
+            pass
+        except Exception as e:
+            print(e)
+
+    def showALLTable(self):
+        try:
+            SQL = "SHOW TABLES"
+            self.executeSQLCursor(SQL)
         except Exception as e:
             print(e)
 
     def addTableColumn(self, table_name, table_column):
         SQL = """
-            IF NOT EXISTS( 
+            IF NOT EXISTS
+            ( 
                 SELECT
                     column_name
                 FROM
                     INFORMATION.SCHEMA.COLUMNS
                 WHERE
                     TABLE_NAME = '{name}'
-                    AND COLUMN_NAME = '{column_name}')
-                ALTER TABLE {name} ADD ({column_name} {column_type});""".format(name=table_name, column_name=table_column[0], column_type = table_column[1])
-        self.cursor.execute(SQL)
+                    AND COLUMN_NAME = '{column_name}'
+            )
+                BEGIN
+                    ALTER TABLE {name} ADD ({column_name} {column_type})
+            END;                    
+            """.format(name=table_name, column_name=table_column[0], column_type=table_column[1])
+
+        self.executeSQLCursor(SQL)
+
+    def executeSQLCursor(self, command):
+        cursor = self.db.cursor()
+        cursor.reset()
+        cursor.execute(command)
+        res = cursor.fetchall()
+        cursor.close()
+        return res
+
+    def __checkTableExist__(self, table_name):
+        SQL = "SHOW TABLES LIKE '{name}'".format(name=table_name)
+        res = self.executeSQLCursor(SQL)
+        if len(res) == 0:
+            raise UnknownTable("Table cannot be found")
+        else:
+            raise TableAlreadyCreated("Table already created")
 
     def __connectToSQL__(self, db_exist=True):
         with open("MySQLcredentials.json") as handler:
@@ -79,5 +97,9 @@ class emailDB:
 class UnknownDatabase(Exception):
     pass
 
+
 class UnknownTable(Exception):
+    pass
+
+class TableAlreadyCreated(Exception):
     pass
