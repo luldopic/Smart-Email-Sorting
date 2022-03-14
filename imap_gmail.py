9d18da544672df8bdebe3628dff0167d55b82a2c
Ludovic Picard <luldopic@googlemail.com>
2022-03-14 20:59:58 +0000

    Update imap_gmail.py

--- imap_gmail.py
+++ imap_gmail.py
@@ -1,7 +1,7 @@
 # Connect to Gmail Server and Fetch Mail
 
 
-from imap_tools import MailBox
+from imap_tools import MailBox, U, A
 import json
 
 
@@ -43,7 +43,7 @@ class gmail(MailBox):
             super().logout()
             self.loggedIn = False
     
-    def fetchMail(self, fetchFrom="latest", fetchNumber="ALL",bulk=False):
+    def fetchMail(self, fetchFrom="latest", fetchNumber="ALL", bulk=False, UID_RANGE = None):
         self.folder.set('Inbox')
         if fetchNumber == "ALL":
             nlimit = None
@@ -55,6 +55,7 @@ class gmail(MailBox):
             reversebool = False
 
         msglist = []
-        for msg in self.fetch(limit=nlimit, reverse=reversebool, bulk=bulk):
+        for msg in self.fetch(A(U(UID_RANGE[0],UID_RANGE[1])), limit=nlimit, reverse=reversebool, bulk=bulk):
             msglist.append(msg)
+            print(msg.uid)
         return msglist
